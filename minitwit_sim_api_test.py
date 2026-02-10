c3350409cac4f161a88b7eccb1af4a7ed0c00f41
TimHounsgaard <timogco@gmail.com>
2026-02-10 14:52:47 +0100

    set up so we can start creating test co-authored: Maja-Box

--- minitwit_sim_api_test.py
+++ minitwit_sim_api_test.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from contextlib import closing
 
 
-BASE_URL = 'http://127.0.0.1:5001'
+BASE_URL = 'http://127.0.0.1:8080'
 DATABASE = "/tmp/minitwit.db"
 USERNAME = 'simulator'
 PWD = 'super_safe!'
@@ -30,7 +30,7 @@ def init_db():
 Path(DATABASE).unlink()
 init_db()
 
-
+"""
 def test_latest():
     # post something to update LATEST
     url = f"{BASE_URL}/register"
@@ -75,7 +75,7 @@ def test_create_msg():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 2
-
+"""
 
 def test_get_latest_user_msgs():
     username = 'a'
@@ -96,7 +96,7 @@ def test_get_latest_user_msgs():
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 3
 
-
+"""
 def test_get_latest_msgs():
     username = 'a'
     query = {'no': 20, 'latest': 4}
@@ -195,4 +195,4 @@ def test_a_unfollows_b():
     # verify that latest was updated
     response = requests.get(f'{BASE_URL}/latest', headers=HEADERS)
     assert response.json()['latest'] == 11
-
+"""
