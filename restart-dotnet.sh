df486439b699824fce6e220f9163b76cd2eff61c
Karam <karamhaybout@gmail.com>
2026-03-11 11:57:07 +0100

    Linting fixes

--- restart-dotnet.sh
+++ restart-dotnet.sh
@@ -7,13 +7,13 @@ FRONTEND_PROJECT_PORT=9002
 
 
 function restart_server {
-	touch $1/dotnet.log
-	nohup dotnet run --project $1 > $1/dotnet.log &
-	
-	# if ! (ps aux | grep frontail)
-	# then
-		nohup frontail $1/dotnet.log -p $2 &
-	# fi
+  touch "$1"/dotnet.log
+  nohup dotnet run --project "$1" > "$1"/dotnet.log &
+  
+  # if ! (ps aux | grep frontail)
+  # then
+   nohup frontail "$1"/dotnet.log -p "$2" &
+  # fi
 }
 
 killall dotnet
@@ -21,22 +21,3 @@ killall frontail
 
 restart_server $FRONTEND_PROJECT $FRONTEND_PROJECT_PORT
 restart_server $API_PROJECT $API_PROJECT_PORT
-
-
-
-
-
-# Add Docker's official GPG key:
-# sudo apt-get update
-# sudo apt-get install ca-certificates curl
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# # Add the repository to Apt sources:
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update
-# sudo apt install docker-compose-plugin
