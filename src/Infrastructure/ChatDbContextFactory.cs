c0baccab27b6574f8ffcfde960052a5c64646dc7
Karam <karamhaybout@gmail.com>
2026-03-11 23:14:22 +0100

    Create ChatDbContextFactory.cs

---
+++ src/Infrastructure/ChatDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure;
+
+public class ChatDbContextFactory : IDesignTimeDbContextFactory<ChatDbContext>
+{
+    public ChatDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ChatDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Database=design-time;Username=dummy;Password=dummy");
+        return new ChatDbContext(optionsBuilder.Options);
+    }
+}
